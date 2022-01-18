b7aae21e87814cc4a0224e07bdcda2fbcc19efa7
andri <andriwinanda98@gmail.com>
2022-01-18 11:31:11 +0700

    update

--- src/components/NewMap.vue
+++ src/components/NewMap.vue
@@ -36,11 +36,15 @@
 <script>
 import L from 'leaflet';
 import "leaflet/dist/leaflet.css";
+import 'leaflet.markercluster/dist/leaflet.markercluster.js';
+import 'leaflet.markercluster/dist/MarkerCluster.css'
+import 'leaflet.markercluster/dist/MarkerCluster.Default.css'
 import "leaflet-rotatedmarker/leaflet.rotatedMarker.js"
 import routeMap from "../routeMap.json";
 import { LineChart } from 'vue-chart-3';
 import { Chart, registerables } from 'chart.js';
-
+import Centrifuge from "centrifuge"
+var YOUR_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJzdGFydGRlbW8iLCJleHAiOjE2NDM5NTkwNDh9.OBdfOPKD4VwhqLqmYO-0jz2AwyL6_9pRIO7zLXdjQHg'
 var index = 0
 var vehicle = null
 Chart.register(...registerables);
@@ -62,6 +66,7 @@ export default {
         iconUrl: 'https://cdn.pixabay.com/photo/2014/04/02/16/28/car-307371_1280.png',
         iconSize: [14, 28],
       },
+      marker: [],
       chartData: {
         datasets: [{
           data: [],
@@ -69,13 +74,18 @@ export default {
           label: 'Speed m/s'
         }],
         labels: []
-      }
+      },
+      colors: ['#007bff', '#37c936', '#868e96', '#ff8f00', '#d81900'],
+      clusteredPoints: null,
+      markers: {}
+
     }
   },
 
   mounted () {
     this.loadPolyline()
     this.createMap()
+    this.centrifugeTest()
   },
 
   beforeDestroy () {
@@ -83,8 +93,54 @@ export default {
   },
 
   methods: {
+    centrifugeTest () {
+      var centrifuge = new Centrifuge('ws://206.189.42.13:8000/connection/websocket');
+      centrifuge.setToken(YOUR_TOKEN);
+
+      var callbacks = {
+        "publish": (message) => {
+          // See below description of message format
+          console.log("publish", message);
+          this.createMarker(message)
+        },
+        "join": function (message) {
+          // See below description of join message format
+          console.log("join", message);
+        },
+        "leave": function (message) {
+          // See below description of leave message format
+          console.log("leave", message);
+        },
+        "subscribe": (context) => {
+          // See below description of subscribe callback context format
+          console.log("subscribe", context);
+        },
+        "error": function (errContext) {
+          // See below description of subscribe error callback context format
+          console.log("error", errContext);
+        },
+        "disconnect": function (errContext) {
+          // See below description of subscribe error callback context format
+          console.log("disconnect", errContext);
+        },
+        "connect": function (context) {
+          console.log("connect", context);
+        },
+        "unsubscribe": function (context) {
+          // See below description of unsubscribe event callback context format
+          console.log("unsubscribe", context);
+        }
+      }
+      centrifuge.on('disconnect', function (context) {
+        // do whatever you need in case of disconnect from server
+        console.log("disconnect", context)
+      });
+      centrifuge.subscribe("positions:startdemo", callbacks);
+      centrifuge.connect();
+
+    },
     createMap () {
-      this.map = L.map('mapContainer').setView(this.center, this.zoom, { animation: true });
+      this.map = L.map('mapContainer').setView(this.center, this.zoom, { zoomControl: true, zoomAnimation: false, fadeAnimation: true, markerZoomAnimation: true });
       L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(this.map);
       L.polyline(this.polyLine, { color: 'green' }).addTo(this.map)
       let carIcon = L.icon({ iconUrl: this.iconOption.iconUrl, iconSize: this.iconOption.iconSize });
@@ -93,6 +149,72 @@ export default {
         rotationAngle: 0,
         rotationOrigin: "center"
       }).addTo(this.map);
+
+
+
+    },
+    iconCar (status) {
+      let indicator = 0
+      if (status === 'PARK') indicator = 1
+      else if (status === 'DRIVING') indicator = 2
+      else if (status === 'STOP') indicator = 3
+      else if (status === 'INACTIVE') indicator = 4
+      else if (status === 'ALERT') indicator = 5
+      let url = `https://start-dev.oss-ap-southeast-5.aliyuncs.com/files/icon-images/sedan0${indicator}_64.png`
+      return L.icon({ iconUrl: url, iconSize: [28, 28], });
+    },
+
+    createMarker (data) {
+      this.clusteredPoints = L.markerClusterGroup({
+        spiderfyOnMaxZoom: true,
+        showCoverageOnHover: true,
+        removeOutsideVisibleBounds: true,
+        iconCreateFunction: (cluster) => {
+          let option = {
+            mag1: 0,
+            mag2: 0,
+            mag3: 0,
+            mag4: 0,
+            mag5: 0
+          }
+          cluster.getAllChildMarkers().map(el => {
+            if (el.options.information.status === 'DRIVING') option.mag1++
+            else if (el.options.information.status === 'STOP') option.mag2++
+            else if (el.options.information.status === 'PARK') option.mag3++
+            else if (el.options.information.status === 'INACTIVE') option.mag4++
+            else if (el.options.information.status === 'ALERT') option.mag5++
+          })
+          return L.divIcon({
+            html: this.createDonutChart(option)
+          });
+        }
+      });
+
+      let customMarker = L.Marker.extend({
+        options: {
+          information: {},
+          setInformation: function (context) { this.information = context },
+          getInformation: function () { return this.information }
+        }
+      });
+
+      data.data.map(el => {
+        if (!this.markers[el.gps_id]) {
+          this.markers[el.gps_id] = new customMarker([el.latitude, el.longitude], {
+            title: el.gps_id,
+            rotationOrigin: "center"
+          })
+          this.clusteredPoints.addLayer(this.markers[el.gps_id]);
+        }
+
+        this.markers[el.gps_id].setRotationAngle(el.course)
+        this.markers[el.gps_id].setIcon(this.iconCar(el.status))
+        this.markers[el.gps_id].setLatLng([el.latitude, el.longitude])
+        this.markers[el.gps_id].options.setInformation(el)
+        // this.clusteredPoints.refreshClusters()
+      })
+      this.map.addLayer(this.clusteredPoints)
+
     },
 
     loadPolyline () {
@@ -146,7 +268,74 @@ export default {
     resetChartData () {
       this.chartData.datasets[0].data = []
       this.chartData.labels = []
+    },
+
+
+    // code for creating an SVG donut chart from feature properties
+    createDonutChart (props) {
+      const offsets = [];
+      const counts = [
+        props.mag1,
+        props.mag2,
+        props.mag3,
+        props.mag4,
+        props.mag5
+      ];
+      let total = 0;
+      for (const count of counts) {
+        offsets.push(total);
+        total += count;
+      }
+      const fontSize =
+        total >= 1000 ? 22 : total >= 100 ? 20 : total >= 10 ? 18 : 16;
+      const r =
+        total >= 1000 ? 50 : total >= 100 ? 32 : total >= 10 ? 24 : 18;
+      const r0 = Math.round(r * 0.6);
+      const w = r * 2;
+
+      let html = `<div>
+      <svg width="${w}" height="${w}" viewbox="0 0 ${w} ${w}" text-anchor="middle" style="font: ${fontSize}px sans-serif; display: block">`;
+
+      for (let i = 0; i < counts.length; i++) {
+        html += this.donutSegment(
+          offsets[i] / total,
+          (offsets[i] + counts[i]) / total,
+          r,
+          r0,
+          this.colors[i]
+        );
+      }
+      html += `<circle cx="${r}" cy="${r}" r="${r0}" fill="white" />
+      <text dominant-baseline="central" transform="translate(${r}, ${r})">
+      ${total.toLocaleString()}
+      </text>
+      </svg>
+      </div>`;
+
+      const el = document.createElement('div');
+      el.innerHTML = html;
+      return el.firstChild;
+    },
+
+    donutSegment (start, end, r, r0, color) {
+      if (end - start === 1) end -= 0.00001;
+      const a0 = 2 * Math.PI * (start - 0.25);
+      const a1 = 2 * Math.PI * (end - 0.25);
+      const x0 = Math.cos(a0),
+        y0 = Math.sin(a0);
+      const x1 = Math.cos(a1),
+        y1 = Math.sin(a1);
+      const largeArc = end - start > 0.5 ? 1 : 0;
+
+      // draw an SVG path
+      return `<path d="M ${r + r0 * x0} ${r + r0 * y0} L ${r + r * x0} ${r + r * y0
+        } A ${r} ${r} 0 ${largeArc} 1 ${r + r * x1} ${r + r * y1} L ${r + r0 * x1
+        } ${r + r0 * y1} A ${r0} ${r0} 0 ${largeArc} 0 ${r + r0 * x0} ${r + r0 * y0
+        }" fill="${color}" />`;
     }
+
+
+
   }
 }
 </script>
